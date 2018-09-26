bac7cda7efdc7bae728281e577c51c993b73ebd7
Callum Hedley <callum.hedley@outlook.com>
2018-09-26 15:27:14 +0100

    added comments

--- bookFinder1.0.1.py
+++ bookFinder1.0.1.py
@@ -1,36 +1,30 @@
+#python script that uses selenium to go to different webpages and return book titles along with a download link that will open up a web page and allow the user to download the book
 import selenium.webdriver as webdriver
 import sys
+#list of sites to get books from
 sites = ["http://b-ok.org","http://gen.lib.rus.ec/"]
+#initalising list variables
 results = []
 name_results = []
 no_digits = []
-links_config='Enabled'
-def options():
-    print('\n*******\nOptions\n*******\n')
-    optionChoice = input('[1]links\n[2]sources\n\nInput a number: ')
-    if optionChoice == '1':
-        print('\n*****\nLinks\n*****\n')
-        print('[1]Links [{}]'.format(links_config))
-        toggle = input('\nInput 1 to toggle: ')
-        if toggle == '1':
-            links_config = 'Disabled'
-    if optionChoice == '2':
-        print('\n*******\nSources\n*******\n')
-        for i in sites:
-            print('[{}]{}'.format(sites.index(i),i))
-        input('\nInput a number to toggle the source on or off: ')
+#getting book results for a users search        
 def getresults(search):
+    #a loop that will loop through the sites in the list above
     for i in sites:
         url = i
         browser = webdriver.PhantomJS()
         browser.get(i)
+        #function that takes in a search element from a webpage(find) and then searches for the book (trueurl)
         def searchBox(find, trueurl):
             searchbox = browser.find_element_by_id(find)
             searchbox.send_keys('{}'.format(trueurl))
             searchbox.submit()
+        #gets the element id name depending on the site   
         if i == "http://b-ok.org":searchId = 'searchFieldx'
         if i == "http://gen.lib.rus.ec/":searchId = 'searchform'
+        #enters the users book into the search
         searchBox(searchId, search)
+        #grabs the downloadlinks and book names from the results page
         if i == "http://b-ok.org":
             webname='bookzz'
             links = browser.find_elements_by_class_name('tdn')
@@ -41,51 +35,48 @@ def getresults(search):
             NamesElement = '//a[text()="[1]"]//parent::td//preceding-sibling::td[@width]//a'
             links = browser.find_elements_by_xpath(LinksElement)
             names = browser.find_elements_by_xpath(NamesElement)
-        
+        #appends downloadlinks and book titles to lists
         for link in links:
             href = link.get_attribute("href")
             results.append(href)
         for name in names:
             n = '[{}]{}'.format(webname,name.text)
             name_results.append(n)
-        no_digits=name_results
-   
+        #closes the selenium browser
         browser.quit()
-    a,b,running,dw=0,5,True,False
+    startNum,endNum,running,downloadPage=0,5,True,False
+    #displays the search results with info on source and options for pages
     while running:
         num=1
         print('\n\n*******\nResults\n*******\n')
-        for i in results[a:b]:
-            print('\n[{}]{}\n{}\n'.format(num, no_digits[results.index(i)], i))
+        for i in results[startNum:endNum]:
+            print('\n[{}]{}\n{}\n'.format(num, name_results[results.index(i)], i))
             num += 1
-        choice = input('[6]Next Page\n[7]Quit\n[8]Options\n\nInput a number: ')
+        choice = input('[6]Next Page\n[7]Quit\n\nInput a number: ')
         for i in results:
-            if choice == '8':
-                options()
-                break
             if choice == str(results.index(i)+1):
                 print('your choice is {}'.format(i))
                 url=i
-                running,dw = False,True
+                running,downloadPage = False,True
                 break
             if choice == '6':
-                a,b=b,b+5
+                startNum,endNum=endNum,endNum+5
                 break
             if choice == '7':
                 running=False
                 break
-    if dw == True:
+    #opens the download page after selection
+    if downloadPage == True:
         browser = webdriver.Chrome()
         browser.get(url)
+#opens main menu with options to quit or search
 def main():
     while True:
         print('\n*********\nMain Menu\n*********\n')
-        startup = input('[1]Start Search\n[2]Options\n[3]quit\n\nInput a number: ')
+        startup = input('[1]Start Search\n[2]quit\n\nInput a number: ')
         if startup == '1':
             getresults(input('\nenter the book you want to read: '))
         if startup == '2':
-            options()
-        if startup == '3':
             sys.exit()
 if __name__=='__main__':
     main()
